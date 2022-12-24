4d630f6288730b728560f715906304d0ee92b2a6
Waleed Elwakeel <waleed_elwakeel13@hotmail.com>
2022-12-24 16:20:24 +0200

    Add project files.

---
+++ RedhatBackend/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedhatBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet("Name")]
+        public IActionResult GetName([FromQuery] string name)
+        {
+            return Ok($"From BE: Name is {name}");
+        }
+    }
+}
